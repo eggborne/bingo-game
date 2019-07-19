828d6b8949d2b14b54fcdd92510eb04cde0fde6f
Mike Donovan <mike@mikedonovan.dev>
2019-07-18 19:45:06 -0700

    Add placeholder images for locked items

--- src/components/MarkerSelectModal.jsx
+++ src/components/MarkerSelectModal.jsx
@@ -2,26 +2,40 @@ import React from 'react';
 import '../css/MarkerSelectModal.css';
 import { chipImages } from '../App';
 
+const lockImage = require('../assets/lockicon.png')
+
 function MarkerSelectModal(props) {
 	console.log('chipim', chipImages)
 	console.log('props.chipimage', props.chipImage)
   let modalClass = '';
   if (!props.showing) {
     modalClass += ' hidden';
-  }
+	}
+	let chipImageNames = Object.keys(chipImages);
+	let lockedMarkers = new Array(30-chipImageNames.length).fill(undefined);
+	let markerList = [...chipImageNames, ...lockedMarkers];
+	console.log('lockedMarkers', lockedMarkers);
   return (
     <div id='marker-select-modal' className={modalClass}>
 			<header>SELECT MARKER</header>
 			<div id='marker-selection'>
-				{Object.keys(chipImages).map(image => {
-					console.log('image?', image);
-					let chipImage = chipImages[image];
-					return (
-						<img key={image} onPointerDown={() => props.onSelectNewMarker(image)} src={chipImage} className={image === props.chipImage ? 'current' : undefined} />
-					);
+				{markerList.map((imageName, i) => {
+					console.log('image?', imageName);
+					if (imageName) {
+						let chipImage = chipImages[imageName];
+						return (
+							<img key={imageName} onPointerDown={() => props.onSelectNewMarker(imageName)} src={chipImage} className={imageName === props.chipImage ? 'current' : undefined} />
+						);
+					} else {
+						return (
+							<img className='locked' key={i} src={lockImage} />
+						);
+					}
 			})}
 			</div>
-			<button onClick={props.onClickOkayButton} className='modal-button' id='marker-ok-button'>OK</button>
+			<div id='button-area'>
+				<button onClick={props.onClickOkayButton} className='modal-button' id='marker-ok-button'>OK</button>
+			</div>
     </div>
   );
 }
